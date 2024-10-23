5d0f28cfcd716f1e8b276b49acd0653a2ca311c2
Pavan <rogerio_pavan@hotmail.com>
2024-10-23 11:38:27 -0300

    feat: add jan validation

--- validators.test.js
+++ validators.test.js
@@ -46,6 +46,52 @@ const cases = [
     value: "978020137962",
     expected: false,
   },
+
+  {
+    symbology: "jan_8",
+    value: "12345",
+    expected: false,
+  },
+  {
+    symbology: "jan_13",
+    value: "invalid",
+    expected: false,
+  },
+  {
+    symbology: "jan_8",
+    value: "96385074",
+    expected: true,
+  },
+  {
+    symbology: "jan_13",
+    value: "7654321",
+    expected: false,
+  },
+  {
+    symbology: "jan_13",
+    value: "96385074",
+    expected: false,
+  },
+  {
+    symbology: "jan_13",
+    value: "invalid",
+    expected: false,
+  },
+  {
+    symbology: "jan_13",
+    value: "1493221968850",
+    expected: true,
+  },
+  {
+    symbology: "jan_13",
+    value: "1122261900581",
+    expected: true,
+  },
+  {
+    symbology: "jan_13",
+    value: "978020137962",
+    expected: false,
+  },
 ];
 
 describe("barcode-validator", () => {
--- validators/index.js
+++ validators/index.js
@@ -4,4 +4,6 @@ const ean_13 = require("./ean13.js");
 module.exports = {
   ean_8,
   ean_13,
+  jan_8: ean_8,
+  jan_13: ean_13,
 };
