3fcd4f08ffe79bbdea3232dae28008eddf0f8ff1
Pavan <rogerio_pavan@hotmail.com>
2024-10-23 19:34:47 -0300

    feat: add upc_a and upc_e in the types

--- barcode-validation.d.ts
+++ barcode-validation.d.ts
@@ -1,4 +1,12 @@
-type Symbology = "ean_13" | "ean_8" | "jan_13" | "jan_8" | "isbn" | "issn";
+type Symbology =
+  | "ean_13"
+  | "ean_8"
+  | "jan_13"
+  | "jan_8"
+  | "isbn"
+  | "issn"
+  | "upc_a"
+  | "upc_e";
 
 declare module "barcode-validation" {
   export function isValid(symbology: Symbology, value: string): boolean;
